0b8550b4f46873d7e4a8c995672cdbdd3c113fe2
Kunyoung Heo <notegrapher@gmail.com>
2018-01-19 03:58:49 +0000

    printFlipInteger.c

---
+++ printFlipInteger.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void printFlip(int input);
+
+void main(void){
+	int input = 0;
+
+	printf("printFilp integer \n");
+	printf("input the number : ");
+	
+	scanf("%d", &input);
+	printFlip(input);
+
+}
+
+void printFlip(int input){
+	int remain = 0;
+
+	while(1){
+		remain = input % 10;
+
+		printf("%d", remain);
+
+		if(input / 10 == 0){
+			printf("\n");
+			break;
+		}
+
+		input = input / 10;
+	}
+}
