e16dad75e642f61c261c8c86187d8e3d90d10222
notegrapher <heo.geoyeo@gmail.com>
2018-01-19 04:11:28 +0000

    printFlipInteger

--- printFlipInteger.c
+++ printFlipInteger.c
@@ -17,7 +17,8 @@ void printFlip(int input){
 	int remain = 0;
 
 	/*
-
+	// 문제 주어졌을 때 내가 해결한 방식
+	
 	while(1){
 		remain = input % 10;
 
@@ -33,7 +34,11 @@ void printFlip(int input){
 
 	*/
 
-	// Recursive로 구현해볼것
+	// 위의 방법은 Recursive인가요 아닌가요?
+	// Answer) 비슷해보이지만 다르다
+	// Why) 재귀함수의 호출 횟수가 많아지는 것 != 지역변수에 큰 숫자 입력하는 것
+	// 그럼 새로운 질문, Recursive로 구현해볼까요?
+
 	remain = input % 10;
 	printf("%d", remain);
 
