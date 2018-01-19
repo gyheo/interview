52e8126e87c441db94fbac15697340c5955f0645
notegrapher <heo.geoyeo@gmail.com>
2018-01-19 04:08:00 +0000

    printFlipInteger

--- printFlipInteger.c
+++ printFlipInteger.c
@@ -16,6 +16,8 @@ void main(void){
 void printFlip(int input){
 	int remain = 0;
 
+	/*
+
 	while(1){
 		remain = input % 10;
 
@@ -28,4 +30,16 @@ void printFlip(int input){
 
 		input = input / 10;
 	}
+
+	*/
+
+	// Recursive로 구현해볼것
+	remain = input % 10;
+	printf("%d", remain);
+
+	if(input / 10 == 0){
+		printf("\n");
+		return ;
+	}
+	printFlip(input / 10);
 }
